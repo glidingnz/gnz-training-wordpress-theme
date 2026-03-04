cafd854910f137eaeb9a59091902ad0c94ff71b0
Will Verland <will@ver.land>
2026-03-04 18:36:47 +1300

    Remove copyright from footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
             <footer style="margin-top: auto; padding: 1rem; border-top: 1px solid #ccc; text-align: center; font-size: 0.85rem; color: #666;">
-                © 2026 Gliding New Zealand Incorporated. To share feedback and report issues, email <a href="mailto:webmaster@gliding.co.nz" style="color: #666;">webmaster@gliding.co.nz</a>.
+                Report issues to <a href="mailto:webmaster@gliding.co.nz" style="color: #666;">webmaster@gliding.co.nz</a>
             </footer>
         </main>
     </div>
